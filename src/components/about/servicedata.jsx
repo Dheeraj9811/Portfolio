e61239bcd9154ebf637db98683e8b28ccb75c491
Dheeraj <88787692+Dheeraj9811@users.noreply.github.com>
2024-09-26 15:53:28 +0530

    Bugfix/update info (#1)
    * updated information
    
    * updated about me

--- src/components/about/servicedata.jsx
+++ src/components/about/servicedata.jsx
@@ -3,29 +3,36 @@ import { CgController } from "react-icons/cg";
 
 
 const servicedata =[
-    {   
-        id:1,
-        title:"Web Design",
-        icon:<CgController />,
-        description:"Create modern, responsive web applications using React for the frontend and Python (Django or Flask) or JavaScript (Express js)for the backend. Deliver seamless user experiences with real-time features and interactive interfaces."
-    },
-
+    
     {
-        id:2,
+        id:1,
         title:"Machine Learning and Data Visualization",
         icon:<CgController />,
         description:"Build machine learning models for regression, classification, and clustering using Python. Transform raw data into meaningful insights, enhancing decision-making processes for clients."
-    }
-    ,
+    },
+
+    {   
+        id:2,
+        title:"Robotics / Perception / ROS",
+        icon:<CgController />,
+        description:"Perception Systems: Engineered advanced perception modules for autonomous robotics and automation, including pallet detection for warehouse management and safety zone monitoring for robot-human interaction, using YOLOv for real-time object detection."
+    },
+    
     {
         id:3,
         title:"Deep Learning",
         icon:<CgController />,
         description:"Build deep learning models using Python and Pytorch to solve complex problems such as image classification, object detection, Nlp and many more."
     },
+    {   
+        id:4,
+        title:"Web Design",
+        icon:<CgController />,
+        description:"Create modern, responsive web applications using React for the frontend and Python (Django or Flask) or JavaScript (Express js)for the backend. Deliver seamless user experiences with real-time features and interactive interfaces."
+    },
 
     {
-        id:4,
+        id:5,
         title:"Api Development",
         icon:<CgController />,
         description:"Build and deploy APIs using Python to enable seamless communication between the frontend and backend of web applications."
--- src/components/project/projectdata.jsx
+++ src/components/project/projectdata.jsx
@@ -3,50 +3,57 @@ import React from 'react'
 const projectdata = [
     {
         id:0,
+        title:"Speaker-Diarization-and-Transcription-Toolkit",
+        discribtion:"VoiceVerse is a reliable, secure and innovative platform revolutionizing the way we interact with audio and video content. With cutting-edge technology, VoiceVerse offers advanced features like audio/video diarisation, allowing users to effortlessly organize and navigate through their recordings. Additionally, our text-to-speech functionality breaks language barriers by providing accurate subtitles in multiple languages. Whether you're a seasoned user or new to the scene, VoiceVerse empowers everyone to engage with audio and video content seamlessly",
+        link:"https://github.com/Dheeraj9811/Speaker-Diarization-and-Transcription-Toolkit"
+    },
+
+    {
+        id:1,
         title:"DetectNet-Web",
         discribtion:"DetectNet-web is your ultimate tool for effortless object detection, powered by ReactJS. This sleek web app is designed to run smoothly on edge devices, providing real-time detection without the need for extensive infrastructure. With its integrated ML object model, DetectNet-web delivers accurate detection results with lightning speed. Step into the future of object detection technology with DetectNet-web.",
         link:"https://detectnet-web.netlify.app/"
     },
     
     {
-        id:1,
+        id:2,
         title:"Contact Management App-Backend",
         discribtion:"It's a backend solution for managing and organizing contact information. This project provides a robust server-side implementation with API endpoints for user registration, authentication, and contact management. It aims to offer a scalable and customizable solution for applications requiring contact-related functionalities",
         link:"https://github.com/Dheeraj9811/Contact-Management-App"
     },
     {
-        id:2,
+        id:3,
         title:"FreshFolds (website)",
         discribtion:"An E-commerce website with full functionality of cart and payment gateway.",
         link:"https://project-freshfolds.netlify.app/"
     },
     {
-        id:3,
+        id:4,
         title: "Anime Face Generator",
         discribtion: "A GAN model to generate anime faces.",
         link: "https://github.com/Dheeraj9811/AnimeFaceGenerator_Gan"
     },
     {
-        id:4,
+        id:5,
         title: "EcomHarbor DB",
         discribtion: "Designed Database Management System for an e-commerce platform.",
         link: "https://github.com/Dheeraj9811/Online-Retail-Store/tree/main"
     },
     {
-        id:5,
+        id:6,
         title: "Gender Recognition by voice",
         discribtion: "Built a model for accurately predicting the gender of an individual based on voice samples.",
         link: "https://github.com/devKhush/Machine-Learning-Project-Gender-Recognition-From-Voice"
     },
     {
-        id:6,
+        id:7,
         title: "ANPR on wifi camera",
         discribtion: " A model to detect license plate from a wifi camera.",
         link: "https://github.com/Dheeraj9811/ANPR-Model-For-Liense-plate-Detection"
 
     },
     {
-        id:7,
+        id:8,
         title: "Snake and ladder Game ",
         discribtion: "Redesign the popular Play Store app game Snake and Ladder for desktop using object-oriented and event-driven programming. ",
         link: "https://github.com/pranaygupta25/SnakesAndLadders"
